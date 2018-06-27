be95adc2e07275adbeb91e36de7cd034d47aae37
Peter Jang <peter.x.jang@gmail.com>
2018-06-26 20:30:47 -0500

    Add some actions for individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,14 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jbuilder"
   end
+
+  def first_product_method
+    @product = Product.first
+    render "one_product.json.jbuilder"
+  end
+
+  def sixth_product_method
+    @product = Product.find_by(id: 6)
+    render "one_product.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/all_products_url" => "products#all_products_method"
+  get "/first_product_url" => "products#first_product_method"
+  get "/sixth_product_url" => "products#sixth_product_method"
 end
