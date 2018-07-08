d94ec5851cd8c9e2b399af334b1a77cd2a2c066f
Peter Jang <peter.x.jang@gmail.com>
2018-07-08 11:11:44 -0500

    Use rails association shortcuts

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ApplicationRecord
-  def product
-    Product.find_by(id: product_id)
-  end
+  belongs_to :product
+  # def product
+  #   Product.find_by(id: product_id)
+  # end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,16 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 1..5000 }
 
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+
+  has_many :images
+  # def images
+  #   Image.where(product_id: id)
+  # end
+
   def discounted?
     price < 10
   end
@@ -15,14 +25,6 @@ class Product < ApplicationRecord
 
   def total
     price + tax
-  end
-
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
-
-  def images
-    Image.where(product_id: id)
   end
 
   def image_urls
