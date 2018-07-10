820682cf763bcfb25479d8dfd0b60cfa08fec8af
Peter Jang <peter.x.jang@gmail.com>
2018-07-09 20:04:35 -0500

    Add authentication helper methods (part 3 of cheat sheet)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def current_user
+    if request.headers['Authorization'].present?
+      token = request.headers['Authorization'].split(' ').last
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: 'HS256' }
+        )
+        User.find_by(id: decoded_token[0]["user"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  helper_method :current_user
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
 end
