91bca97164e58048d272b109641a7fe07493bd6f
Peter Jang <peter.x.jang@gmail.com>
2018-07-15 11:48:41 -0500

    Refactor logic into model method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,27 +7,13 @@ class OrdersController < ApplicationController
   end
 
   def create
-    carted_products = current_user.carted_products.where(status: "carted")
-    calculated_subtotal = 0
-    calculated_tax = 0
-    calculated_total = 0
-    carted_products.each do |carted_product|
-      calculated_subtotal += carted_product.product.price * carted_product.quantity
-      calculated_tax += carted_product.product.tax * carted_product.quantity
-      calculated_total += carted_product.product.total * carted_product.quantity
-    end
     @order = Order.new(
-      user_id: current_user.id,
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
+      user_id: current_user.id
     )
     @order.save
-    carted_products.each do |carted_product|
-      carted_product.status = "purchased"
-      carted_product.order_id = @order.id
-      carted_product.save
-    end
+    carted_products = current_user.carted_products.where(status: "carted")
+    carted_products.update_all(status: "purchased", order_id: @order.id)
+    @order.update_all_totals
     render "show.json.jbuilder"
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,19 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :carted_products
   has_many :products, through: :carted_products
+
+  def update_all_totals
+    calculated_subtotal = 0
+    calculated_tax = 0
+    calculated_total = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.product.price * carted_product.quantity
+      calculated_tax += carted_product.product.tax * carted_product.quantity
+      calculated_total += carted_product.product.total * carted_product.quantity
+    end
+    self.subtotal = calculated_subtotal
+    self.tax = calculated_tax
+    self.total = calculated_total
+    self.save
+  end
 end
