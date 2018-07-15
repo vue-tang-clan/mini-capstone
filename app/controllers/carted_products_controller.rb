4089c09077fc79dd0d70b360e2564a746d84ff08
Peter Jang <peter.x.jang@gmail.com>
2018-07-15 10:51:36 -0500

    Add carted products index action

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,4 +1,10 @@
 class CartedProductsController < ApplicationController
+  def index
+    @carted_products = current_user.carted_products.where(status: "carted")
+    # @carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
+    render "index.json.jbuilder"
+  end
+
   def create
     @carted_product = CartedProduct.new(
       user_id: current_user.id,
---
+++ app/views/carted_products/index.json.jbuilder
@@ -0,0 +1,1 @@
+json.carted_products @carted_products
